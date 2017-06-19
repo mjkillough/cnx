709af1ad1b2fc7c4e2f1c43ac39c5c226efd8854
Michael Killough <michaeljkillough@gmail.com>
2017-06-19 14:59:06 +0700

    Refactor main(), stepping stone towards #22.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ extern crate xcb;
 
 use std::rc::Rc;
 
-use tokio_core::reactor::Core;
+use tokio_core::reactor::{Core, Handle};
 use tokio_timer::Timer;
 
 mod errors;
@@ -31,41 +31,71 @@ mod bar;
 use bar::*;
 
 
-fn run() -> Result<()> {
-    let mut core = Core::new().chain_err(|| "Could not create Tokio Core")?;
-    let handle = core.handle();
+struct Hue {
+    core: Core,
+    timer: Rc<Timer>,
+    bar: Bar,
+    widgets: Vec<Box<Widget>>,
+}
 
-    let inactive_attr = Attributes {
+impl Hue {
+    pub fn new(position: Position) -> Result<Hue> {
+        Ok(Hue {
+            core: Core::new().chain_err(|| "Could not create Tokio Core")?,
+            timer: Rc::default(),
+            bar: Bar::new(position)?,
+            widgets: Vec::new(),
+        })
+    }
+
+    pub fn handle(&self) -> Handle {
+        self.core.handle()
+    }
+
+    pub fn timer(&self) -> Rc<Timer> {
+        self.timer.clone()
+    }
+
+    pub fn add_widget<W>(&mut self, widget: W) where W: Widget + 'static {
+        self.widgets.push(Box::new(widget) as Box<Widget>);
+    }
+
+    pub fn run(mut self) -> Result<()> {
+        let handle = self.handle();
+        self.core.run(self.bar.run_event_loop(&handle, self.widgets)?)
+    }
+}
+
+
+fn run() -> Result<()> {
+    let attr = Attributes {
         font: pango::FontDescription::from_string("SourceCodePro 21"),
         fg_color: Color::white(),
         bg_color: None,
         padding: Padding::new(8.0, 8.0, 0.0, 0.0),
     };
-    let active_attr = inactive_attr.with_bg_color(Some(Color::blue()));
+    let active_attr = attr.with_bg_color(Some(Color::blue()));
 
-    let sensors = vec!["Core 0", "Core 1"];
-    let timer = Rc::new(Timer::default());
-    let widgets: Vec<Box<Widget>> =
-        vec![
-            Box::new(Pager::new(
-                handle.clone(),
-                active_attr,
-                inactive_attr.clone(),
-            )) as Box<Widget>,
-            Box::new(ActiveWindowTitle::new(
-                handle.clone(),
-                inactive_attr.clone(),
-            )) as Box<Widget>,
-            Box::new(Sensors::new(timer.clone(), inactive_attr.clone(), sensors)) as Box<Widget>,
-            Box::new(Volume::new(handle.clone(), inactive_attr.clone())) as Box<Widget>,
-            Box::new(Battery::new(timer.clone(), inactive_attr.clone())) as Box<Widget>,
-            Box::new(Clock::new(timer.clone(), inactive_attr.clone())) as Box<Widget>,
-        ];
+    let mut hue = Hue::new(Position::Bottom)?;
 
-    let bar = Bar::new(Position::Bottom)?;
-    core.run(bar.run_event_loop(&handle, widgets)?)?;
+    // Yucky macro to get around the fact that hue.add_widget(... hue.handle())
+    // complains about the second immutable borrow. We should be able to get
+    // rid of this if we implement a clever builder pattern for widgets which
+    // Hue can use.
+    macro_rules! add_widget {
+        ($hue:ident, $widget:expr) => {
+            let widget = $widget;
+            $hue.add_widget(widget);
+        }
+    }
+    add_widget!(hue, Pager::new(hue.handle(), active_attr, attr.clone()));
+    add_widget!(hue, ActiveWindowTitle::new(hue.handle(), attr.clone()));
+    add_widget!(hue, Sensors::new(hue.timer(), attr.clone(), vec!["Core 0", "Core 1"]));
+    add_widget!(hue, Volume::new(hue.handle(), attr.clone()));
+    add_widget!(hue, Battery::new(hue.timer(), attr.clone()));
+    add_widget!(hue, Clock::new(hue.timer(), attr.clone()));
 
-    Ok(())
+    hue.run()
 }
 
 quick_main!(run);
