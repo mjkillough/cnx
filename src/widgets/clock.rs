3f0c2d060a587e5100082de190318794921307c4
Michael Killough <michaeljkillough@gmail.com>
2017-06-19 12:32:54 +0700

    Don't wake-up every second for the Clock widget.
    Take advantage of the fact that we do not show 'seconds' and wait
    however many seconds it'll be until the 'minutes' changes before waking
    up.
    
    Fixes #18.

--- src/widgets/clock.rs
+++ src/widgets/clock.rs
@@ -2,15 +2,16 @@ use std::rc::Rc;
 use std::time::Duration;
 
 use chrono::prelude::*;
+use futures::{stream, Future, Stream};
 use tokio_timer::Timer;
 
 use errors::*;
 use text::{Attributes, Text};
+use super::{Widget, WidgetStream};
 
 
 pub struct Clock {
     timer: Rc<Timer>,
-    update_interval: Duration,
     attr: Attributes,
 }
 
@@ -18,21 +19,37 @@ impl Clock {
     pub fn new(timer: Rc<Timer>, attr: Attributes) -> Clock {
         Clock {
             timer,
-            update_interval: Duration::from_secs(1),
             attr,
         }
     }
-
-    fn tick(&self) -> Result<Vec<Text>> {
-        let current_time = Local::now().format("%Y-%m-%d %a %I:%M %p").to_string();
-        Ok(vec![
-            Text {
-                attr: self.attr.clone(),
-                text: current_time,
-                stretch: false,
-            },
-        ])
-    }
 }
 
-timer_widget!(Clock, timer, update_interval, tick);
+impl Widget for Clock {
+    fn stream(self: Box<Self>) -> Result<WidgetStream> {
+        // As we're not showing seconds, we can sleep for however long it takes
+        // until the minutes changes between updates. Initially sleep for 0 seconds
+        // so that our `self.timer.sleep()` expires immediately.
+        let sleep_for = Duration::from_secs(0);
+        let stream = stream::unfold(sleep_for, move |sleep_for| {
+            // Avoid having to move self into the .map() closure.
+            let attr = self.attr.clone();
+            Some(self.timer.sleep(sleep_for)
+                .map(move |()| {
+                    let now = Local::now();
+                    let formatted = now.format("%Y-%m-%d %a %I:%M %p").to_string();
+                    let texts = vec![
+                        Text {
+                            attr: attr,
+                            text: formatted,
+                            stretch: false,
+                        },
+                    ];
+
+                    let sleep_for = Duration::from_secs(60 - now.second() as u64);
+                    (texts, sleep_for)
+                }))
+        }).then(|r| r.chain_err(|| "Error in tokio_timer stream"));
+
+        Ok(Box::new(stream))
+    }
+}
