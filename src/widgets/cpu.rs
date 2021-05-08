6b82740a1e52f98bdbcaf6aec06f73df873823d4
Sibi Prabakaran <sibi@psibi.in>
2021-05-08 19:06:15 +0530

    Refactor cpu widget

--- src/widgets/cpu.rs
+++ src/widgets/cpu.rs
@@ -1,11 +1,13 @@
 use crate::text::{Attributes, Text};
 use crate::widgets::{Widget, WidgetStream};
 use anyhow::{anyhow, Result};
-use async_stream::try_stream;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::time::Duration;
+use tokio_stream::wrappers::IntervalStream;
+use tokio_stream::StreamExt;
+use tokio::time;
 
 pub struct Cpu {
     attr: Attributes,
@@ -21,6 +23,36 @@ impl Cpu {
             cpu_data,
             render,
         })
+    }
+
+    fn tick(&mut self) -> Result<Vec<Text>> {
+        let cpu_data = CpuData::get_values()?;
+
+        // https://github.com/jaor/xmobar/blob/61d075d3c275366c3344d59c058d7dd0baf21ef2/src/Xmobar/Plugins/Monitors/Cpu.hs#L128
+        let previous = &self.cpu_data;
+        let current = cpu_data;
+        let diff_total = (current.user_time - previous.user_time) +
+            (current.nice_time - previous.nice_time) +
+            (current.system_time - previous.system_time) +
+            (current.idle_time - previous.idle_time) +
+            (current.iowait_time - previous.iowait_time) +
+            (current.total_time - previous.total_time);
+        let percentage = match diff_total {
+            0 => 0.0,
+            _ => (current.total_time - previous.total_time) as f64 / diff_total as f64
+        };
+
+        let cpu_usage = (percentage * 100.0) as u64;
+        let text = self.render.as_ref().map_or(format!("{} %", cpu_usage), |x| (x)(cpu_usage).clone());
+        self.cpu_data = current;
+        let texts = vec![Text {
+            attr: self.attr.clone(),
+            text: text.clone(),
+            stretch: false,
+            markup: true
+        }];
+        Ok(texts)
+
     }
 }
 
@@ -74,56 +106,9 @@ impl CpuData {
 
 impl Widget for Cpu {
     fn into_stream(mut self: Box<Self>) -> Result<WidgetStream> {
-        let stream = try_stream! {
-            loop {
-                let cpu_data = CpuData::get_values()?;
-
-                // Based on htop https://stackoverflow.com/a/23376195/1651941
-                let prev_idle = self.cpu_data.idle_time;
-                let prev_non_idle = self.cpu_data.total_time;
-
-                let idle = cpu_data.idle_time;
-                let non_idle = cpu_data.total_time;
-
-                let prev_total = prev_idle + prev_non_idle;
-                let total = idle + non_idle;
-
-                let total_diff = total;
-                let idle_diff = idle;
-
-                // https://github.com/jaor/xmobar/blob/61d075d3c275366c3344d59c058d7dd0baf21ef2/src/Xmobar/Plugins/Monitors/Cpu.hs#L128
-                let previous = self.cpu_data;
-                let current = cpu_data;
-                let diff_total = (current.user_time - previous.user_time) +
-                    (current.nice_time - previous.nice_time) +
-                    (current.system_time - previous.system_time) +
-                    (current.idle_time - previous.idle_time) +
-                    (current.iowait_time - previous.iowait_time) +
-                    (current.total_time - previous.total_time);
-                let percentage = match diff_total {
-                    0 => 0.0,
-                    _ => (current.total_time - previous.total_time) as f64 / diff_total as f64
-                };
-
-                let cpu_usage = (percentage * 100.0) as u64;
-                let text = self.render.map_or(format!("{} %", cpu_usage), |x| x(cpu_usage));
-
-                let texts = vec![Text {
-                    attr: self.attr.clone(),
-                    text,
-                    stretch: false,
-                    markup: true
-                }];
-
-                self.cpu_data = current;
-
-                yield texts;
-
-                let sleep_for = Duration::from_secs(10);
-                tokio::time::sleep(sleep_for).await;
-            }
-        };
-
+        let ten_seconds = Duration::from_secs(10);
+        let interval = time::interval(ten_seconds);
+        let stream = IntervalStream::new(interval).map(move |_| self.tick());
         Ok(Box::pin(stream))
     }
 }
